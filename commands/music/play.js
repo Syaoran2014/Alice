8aef7659f4c55e4531e8ac9d96674143bd73ffda
Syaoran2014 <38767574+Syaoran2014@users.noreply.github.com>
2023-12-01 20:35:55 -0600

    Pipeline bug fixes....

--- commands/music/play.js
+++ commands/music/play.js
@@ -106,7 +106,7 @@ module.exports = {
             const query = song; 
             const res = await player.search(query, {
                 requestedBy: interaction.member,
-                searchEngine: platform ? QueryType.platform : QueryType.YOUTUBE
+                searchEngine: platform ? QueryType.platform : QueryType.SPOTIFY_SEARCH
             });
 
             if(!res || !res.tracks.length) {
